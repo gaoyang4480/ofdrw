656df1d787ec2f754298dcc774d7a0930bdec963
gaoyang <641782191@qq.com>
2020-11-17 11:51:12 +0800

    support image cache

--- ofdrw-layout/src/main/java/org/ofdrw/layout/engine/ResManager.java
+++ ofdrw-layout/src/main/java/org/ofdrw/layout/engine/ResManager.java
@@ -20,7 +20,10 @@ import org.ofdrw.pkg.container.DocDir;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -235,9 +238,43 @@ public class ResManager {
         }
     }
 
+    public String getSHA256(byte[] data) {
+        MessageDigest messageDigest;
+        String encodestr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(data);
+            encodestr = byte2Hex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encodestr;
+    }
+
+    /**
+     * 将byte转为16进制
+     *
+     * @param bytes
+     * @return
+     */
+    private String byte2Hex(byte[] bytes) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String temp = null;
+        for (int i = 0; i < bytes.length; i++) {
+            temp = Integer.toHexString(bytes[i] & 0xFF);
+            if (temp.length() == 1) {
+                //1得到一位的进行补0操作
+                stringBuffer.append("0");
+            }
+            stringBuffer.append(temp);
+        }
+        return stringBuffer.toString();
+    }
+
     public ST_ID addImage(String imageName, byte[] imgData) throws IOException {
         Res resMenu = docRes();
-        if (cache.get(imageName) == null) {
+        String imgDataHash = getSHA256(imgData);
+        if (cache.get(imgDataHash) == null) {
             if (medias == null) {
                 this.medias = new MultiMedias();
                 resMenu.addResource(medias);
@@ -257,11 +294,11 @@ public class ResManager {
             // 加入媒体类型清单
             medias.addMultiMedia(multiMedia);
             // 加入缓存
-            cache.put(imageName, multiMedia);
+            cache.put(imgDataHash, multiMedia);
             return id;
         } else {
             // 该资源已经加入过返回资源的ID
-            return cache.get(imageName).getObjID();
+            return cache.get(imgDataHash).getObjID();
         }
     }
 
